bf196ada2fd162562ec91745c9aa1ff43a528f84
Alyetama <56323389+Alyetama@users.noreply.github.com>
2022-02-23 02:10:15 -0500

    Remove wall time func

--- helpers.py
+++ helpers.py
@@ -1,16 +1,6 @@
-import datetime
-import os
 import sys
 
 import GPUtil
-
-
-def crude_walltime_limit():
-    TOTAL_NUM_OF_IMGS = int(os.environ['TOTAL_NUM_OF_IMGS'])
-    wt = str(datetime.timedelta(seconds=TOTAL_NUM_OF_IMGS))[:-3]
-    if wt[1] == ':':
-        wt = f'0{wt}'
-    return wt
 
 
 def get_avail_gpus():
@@ -26,7 +16,5 @@ def get_avail_gpus():
 
 
 if __name__ == '__main__':
-    if '--time' in sys.argv:
-        print(crude_walltime_limit())
     elif '--gpus' in sys.argv:
         print(get_avail_gpus())
